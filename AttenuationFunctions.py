8e05b07390f2b3c210ef8b2b0cf288f1d55a1912
hkunwer <114240436+hkunwer@users.noreply.github.com>
2023-09-08 16:34:38 -0400

    Clean up
    Big edits to clean up code. Now its very simple calling all functions and saves dataframes and plots by unique event ID.

--- AttenuationFunctions.py
+++ AttenuationFunctions.py
@@ -1,30 +1,10 @@
-# %%
-import csv
-import numpy as np
-from scipy.fftpack import fft,ifft
-# %%
 import os
 from locale import setlocale
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import sin,cos,arcsin,sqrt,abs,pi,log10,exp
-from scipy.fftpack import fft,ifft
-from scipy.io import wavfile
-from scipy.stats import gmean 
-from tqdm import tqdm
-from compress_pickle import dump as cpkldump # reading/writing compressed pickles
-from compress_pickle import load as cpklload # reading/writing compressed pickles
-
-#obspy
-from obspy import UTCDateTime
-
-from obspy.taup import TauPyModel
-model = TauPyModel(model="iasp91")
-
-#rtergpy
-from rtergpy.run import defaults, event, etime2name, src2ergs
-from rtergpy.waveforms import getwaves, get_respinv
+import csv
+from numpy import abs
 
 def processANSS():
 
@@ -60,69 +40,26 @@ def processANSS():
         writer = csv.writer(file)
         writer.writerows(modified_lines)
 
-# %%
-def freqmaxes(trace, freqs): 
+    ANSS = pd.read_csv('ANSS_processed_data.csv')
     
-    data = trace.data                           # the actual data
-    samp = data.shape[-1]                       # number of sample point
-    delta = trace.stats.delta                  # sample spacing 
-    time = trace.times()       
-                # time axis 
-    # ---- compute fft for our data
-    fftdata = np.fft.rfft(data, n=samp)
-
-    # ---- Fdat has real and complex part, to compute power spectrum we take the
-    fftdata_abs = np.abs(fftdata)
-
-    # ---- compute x-axis in frequency-domain for plotting
-    xf = np.linspace(0.0, 1.0/(2.0*delta), int(samp/2)+1)
-
-    #make variables to store the values in
-    maxamps = [] #max amplitudes here
-    idx_of_max_amps = [] #position of max amps here
-
-    for freq in freqs: 
+    return ANSS
         
-        #argmin find specific position here
-        idx_of_min_freq = (np.abs(xf-freq[0])).argmin()
-        idx_of_max_freq = (np.abs(xf-freq[1])).argmin()
-        
-        fftdata_abs_slice = fftdata_abs[idx_of_min_freq:idx_of_max_freq]
-
-        # ---- get the max value of amplitude spectrum
-        maxamp = fftdata_abs_slice.max()
-        idx_of_max_amp = int(np.where(fftdata_abs_slice == maxamp) + idx_of_min_freq)
-        maxamps.append(1.0/samp * maxamp)
-        idx_of_max_amps.append(idx_of_max_amp)
+def filtering(stream):
     
-    return maxamps, idx_of_max_amps
-# need to fix this function so it actually works when called. Currently doing manually since 
-# shes rude right now
-def maximumAmplitude(stream, frequencymin, frequencymax):
+    taper=0.05
+    stp = stream.copy()  # create backup
+    # process data (maybe make this into a function)
+    stp.detrend(type='polynomial', order=5) # pre-instrument removal
+    stp.taper(taper)
+    stp.remove_response(output="DISP")
+    stp.detrend(type='polynomial', order=5) # post-instrument removal
+    stp.taper(taper)
     
-    stp_freq = stream.copy()
-    stp_freq.filter("bandpass",freqmin=frequencymin , freqmax= frequencymax)
+    return stp
 
-    # stp_freq[0].plot();
-
-    maxamps = [] 
-    dist_str = []
-    station_name = []
-
-    for tr in stp_freq:
-
-        maxamp = np.max(abs(tr))
-        dist = tr.stats.distance / 1000
-        station = tr.stats.station
-        maxamps.append(maxamp)
-        dist_str.append(dist)
-        station_name.append(station)
-        
-    return station_name, maxamps, dist_str
-
-def process_frequency_bands(stream, freqlist):
+def maxamp_calc(stream, eventID):
     
-    #freqlist = [(0.10, 0.25),(0.25, 0.50),(0.50, 0.75),(0.75, 1.00),(1.00, 1.25)]
+    freqlist = [(0.10, 0.25),(0.25, 0.50),(0.50, 0.75),(0.75, 1.00),(1.00, 1.25)]
     maxamps = []
     dist_str = []
     station_name = []
@@ -145,9 +82,10 @@ def process_frequency_bands(stream, freqlist):
             frequencies.append(freq_range)
   
     df_freq = pd.DataFrame({"maxamps":maxamps,"distance":dist_str,"station":station_name, "frequency":frequencies}) 
+    df_freq.to_csv('MaxAmps_'+eventID+'.csv', index=False)
     return df_freq
 
-def organize_data(df_freq, EQ, etime,eloc):          
+def organize_data(df_freq, EQ, etime,eloc, eventID):          
             
     stations = []
     distances = []
@@ -178,6 +116,27 @@ def organize_data(df_freq, EQ, etime,eloc):
         'max amplitude': max_amplitudes,
         'frequency band': frequency_bands
     }
-
     df_final = pd.DataFrame(data)
-    return df_final
+    df_final.to_csv('Results_'+eventID+'.csv', index=False) # Export to CSV
+
+def maxamp_plot(eventID, dataframe):
+    color_mapping = {
+    (0.10, 0.25): 'blue',
+    (0.25, 0.50): 'green',
+    (0.50, 0.75): 'red',
+    (0.75, 1.00): 'orange',
+    (1.00, 1.25): 'purple',}
+    # Create a figure and axis
+    fig, ax = plt.subplots()
+    # Loop through unique frequency bands
+    for freq_range, group in dataframe.groupby('frequency'):
+        color = color_mapping[tuple(freq_range)]  # Get color based on frequency range
+        ax.scatter(group['distance'], group['maxamps'], label = f'{freq_range[0]:.2f}-{freq_range[1]:.2f} Hz',color=color, marker = 'o',facecolors='none')
+    # Add labels, legend, and show the plot
+    plt.yscale('log')
+    plt.title('The Max Amplitude for '+ eventID)
+    ax.set_xlabel('Distance (km)')
+    ax.set_ylabel('Max amplitude (Log scale)')
+    plt.legend()
+    image = plt.savefig('MaxAmps_'+eventID+'.png') #save plot by unique event ID
+    #plt.show()  
